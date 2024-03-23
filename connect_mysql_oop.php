e3cd741a1a2a3dc9c8e8f02196ee666f9797c523
Md Tuhin Mia <mtuhinahmed95@gmail.com>
2024-03-23 23:09:00 +0600

    oop connect mysql

---
+++ connect_mysql_oop.php
@@ -0,0 +1,15 @@
+<?php
+
+$server_name = "localhost";
+$user_name = "root";
+$password = "";
+$db_name = "college";
+
+$conn = new mysqli($server_name,$user_name,$password,$db_name);
+
+if($conn->connect_error){ 
+    die("connect failed".$conn->connect_error);
+}
+echo "connect succesfully";
+
+?>
