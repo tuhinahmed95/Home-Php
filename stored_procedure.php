bf02643a5bfe000e45a90971f6c2746abdd178a8
Md Tuhin Mia <mtuhinahmed95@gmail.com>
2024-03-30 22:45:55 +0600

    update

--- stored_procedure.php
+++ stored_procedure.php
@@ -1,13 +1,13 @@
 <?php
 
-$conn = new mysqli ("localhost","root","","Company");
+$conn = new mysqli ("localhost","root","","company");
 
 if(isset($_POST['btn'])){ 
     $f_name =  $_POST['fname'];
     $a_address = $_POST['address'];
     $c_contact = $_POST['contact'];
 
-    $conn->query("call pro_manufacture('$f_name','$a_address','$c_contact')");
+    $conn->query("call pro_manufacturer('$f_name','$a_address','$c_contact')");
 }
 
 ?>
@@ -48,8 +48,10 @@ if(isset($_POST['btnsub'])){
     $name = $_POST['name'];
     $price = $_POST['price'];
     $menu = $_POST['manu'];
+    $manufac = $_POST['menufac'];
 
-    $conn->query("call pro_product('$name','$price','$menu')");
+    
+    $conn->query("call pro_product('$name','$price','$menu','$manufac')");
 }
 
 ?>
@@ -78,7 +80,7 @@ if(isset($_POST['btnsub'])){
            <select name="menufac"> 
             <?php
 
-            $menufac =$conn->query("select * From manufacture");
+            $menufac =$conn->query("SELECT * FROM manufacturer");
             while(list($mid,$name)=$menufac->fetch_row()){ 
                 echo "<option value='$mid'>$name</option>";
             }
@@ -102,27 +104,31 @@ if(isset($_POST['btnsub'])){
 <table border="1" style="border-collapse: collapse;"> 
 
             <tr> 
-                <td>Id</td>
-                <td>Name</td>
-                <td>Price</td>
-                <td>Mname</td>
-                <td>Address</td>
-                <td>Contact</td>
+                <td><strong>ID</strong></td>
+                <td><strong>Name</strong></td>
+                <td><strong>Address</strong></td>
+                <td><strong>Contact</strong></td>
+                <td><strong>P Name</strong></td>
+                <td><strong>Price</strong></td>
+                <td><strong>Manufacture Id</strong></td>
+                
+
             </tr>
 
             <?php
-            $product = $conn->query("select * from vie_product");
-            while(list($id,$name,$price,$mname,$address,$contact) = $product->fetch_row()){ 
+            $product = $conn->query("SELECT * FROM vie_product");
+            while(list($id,$name,$address,$contact,$pname,$price,$manu) = $product->fetch_row()){ 
 
                 echo "<tr> 
                 
                             <td>$id</td>
                             <td>$name</td>
-                            <td>$price</td>
-                            <td>$mname</td>
                             <td>$address</td>
                             <td>$contact</td>
-                
+                            <td>$pname</td>
+                            <td>$price</td>
+                            <td>$manu</td>
+
                     </tr>";
             }
 
