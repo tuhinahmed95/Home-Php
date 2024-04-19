d8b2a1f36bfa65637ed230abc32a16bb3a7adbcb
Md Tuhin Mia <mtuhinahmed95@gmail.com>
2024-04-19 17:13:06 +0600

    up

--- php_mysql/crud_operation/insert.php
+++ php_mysql/crud_operation/insert.php
@@ -32,29 +32,40 @@ if(isset($_POST['btn'])){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Crud Operation</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 <body>
 
-    <form action="insert.php"> 
-        <label for="id">ID 
-            <input type="text" name="id"><br><br>
-        </label><br>
+   <div class="container"> 
+    <div class="row"> 
+        <div class="col-sm-3">
+        </div>
 
-        <label for="name">NAME 
-            <input type="text" name="name"><br><br>
-        </label><br><br>
+        <div class="col-sm-6"> 
 
-        <label for="email">Email 
-            <input type="email" name="email"><br><br>
-        </label><br>
+        <h1>Registration Form</h1>
 
-        <label for="acnu">AC Number 
-            <input type="text" name="acnu"><br><br>
-        </label><br>
+        <form action="insert.php" method="POST"> 
+       
+        ID <br>
+        <input type="text" name="id"><br><br>
+        NAME <br>
+        <input type="text" name="name"><br><br>
+        EMAIL <br>
+        <input type="email" name="email"><br><br>
+        AC NUMBER <br>
+        <input type="text" name="acnu"><br><br>
 
         <input type="submit" name="btn" value="submit">
     </form>
+
+        </div>
+
+        <div class="col-sm-3"> 
+        </div>
+    </div>
+   </div>
     
 </body>
 </html>
