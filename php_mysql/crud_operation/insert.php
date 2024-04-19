d2ab8ae3cc0536a2597808b8228c0cd371c200a6
Md Tuhin Mia <mtuhinahmed95@gmail.com>
2024-04-19 16:53:10 +0600

    up

--- php_mysql/crud_operation/insert.php
+++ php_mysql/crud_operation/insert.php
@@ -1,4 +1,28 @@
+<?php
 
+$connectDB = mysqli_connect ("localhost","root","","bank");
+
+if(isset($_POST['btn'])){ 
+    $id    = $_POST['id'];
+    $name  = $_POST['name'];
+    $email = $_POST['email'];
+    $acnum = $_POST['acnu'];
+    
+    
+    $sql = "INSERT INTO customer(id,name,email,acnumber) VALUES ('$id','$name','$email','$acnum')";
+
+    if(mysqli_query($connectDB,$sql) == TRUE){ 
+        echo "Data Inserted Succesfully";
+        header("location:insert.php");
+    }
+    else{ 
+        echo "Data Is Not Inserted";
+    }
+}
+
+
+
+?>
 
 
 
@@ -21,7 +45,7 @@
             <input type="text" name="name"><br><br>
         </label><br><br>
 
-        <label for="emai">Email 
+        <label for="email">Email 
             <input type="email" name="email"><br><br>
         </label><br>
 
