869871513a3321c727e0ea96665a9346ac46400d
Md Tuhin Mia <mtuhinahmed95@gmail.com>
2024-04-19 20:30:01 +0600

    up

--- php_mysql/crud_operation/insert.php
+++ php_mysql/crud_operation/insert.php
@@ -27,45 +27,10 @@ if(isset($_POST['btn'])){
 
 
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Crud Operation</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-</head>
-<body>
-
-   
      
        
        
 
          
 
-        <h3>Registration Form</h3>
-
-        <form action="insert.php" method="POST"> 
        
-        ID <br>
-        <input type="text" name="id"><br><br>
-        NAME <br>
-        <input type="text" name="name"><br><br>
-        EMAIL <br>
-        <input type="email" name="email"><br><br>
-        AC NUMBER <br>
-        <input type="text" name="acnu"><br><br>
-
-        <input type="submit" name="btn" value="submit" class="btn btn-succes">
-    </form>
-
-        </div>
-
-         
-        
-    
-  
-    
-</body>
-</html>
