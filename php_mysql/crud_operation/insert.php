1e0f8db71d61a40cd121ba17f8ef9aea18c527ac
Md Tuhin Mia <mtuhinahmed95@gmail.com>
2024-04-19 16:40:03 +0600

    update

---
+++ php_mysql/crud_operation/insert.php
@@ -0,0 +1,36 @@
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <form action="insert.php"> 
+        <label for="id">ID 
+            <input type="text" name="id"><br><br>
+        </label><br>
+
+        <label for="name">NAME 
+            <input type="text" name="name"><br><br>
+        </label><br><br>
+
+        <label for="emai">Email 
+            <input type="email" name="email"><br><br>
+        </label><br>
+
+        <label for="acnu">AC Number 
+            <input type="text" name="acnu"><br><br>
+        </label><br>
+
+        <input type="submit" name="btn" value="submit">
+    </form>
+    
+</body>
+</html>
