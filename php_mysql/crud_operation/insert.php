b98698ced990a1c0e6fea0b56ed7d02d93812593
Md Tuhin Mia <mtuhinahmed95@gmail.com>
2024-04-19 14:18:49 +0600

    up

---
+++ php_mysql/crud_operation/insert.php
@@ -0,0 +1,23 @@
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Crud Operation</title>
+</head>
+<body>
+
+    <form action="insert.php" method="POST"> 
+        ID <br>
+        <input type="text" name="id"><br><br>
+        NAME <br>
+        <input type="text" name="name"><br><br>
+    </form>
+    
+</body>
+</html>
