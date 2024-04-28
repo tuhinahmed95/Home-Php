ba67f7e0bee4039562309a2c839ac3e9d47955c2
Md Tuhin Mia <mtuhinahmed95@gmail.com>
2024-04-28 22:56:23 +0600

    ordermanagement

---
+++ OrderManagement/order.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Order management</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+    <div class="container"> 
+
+        <header class="header"> 
+            <div class="header-1"> 
+                <h1>Hi,Welcome Back</h1>
+            </div>
+
+            <div class="header-2"> 
+                <h3>431007</h3>
+            </div>
+
+        </header>
+
+        <div class="clerar-div"></div>
+
+       
+       
+    </div>
+    
+</body>
+</html>
