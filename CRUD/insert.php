aacb11b372f940023941ff25d1b2472968fedeb0
Md Tuhin Mia <mtuhinahmed95@gmail.com>
2024-04-30 23:22:07 +0600

    up

--- CRUD/insert.php
+++ CRUD/insert.php
@@ -16,7 +16,7 @@ if (isset($_POST['submit'])){
 
 
 ?>
-
+<!-- this is a login page -->
 <html lang="en">
 <head>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
