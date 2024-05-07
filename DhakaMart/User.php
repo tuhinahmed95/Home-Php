756f1422ecd7fc767baf7a5a798ef4a4c1c0bcfc
Md Tuhin Mia <mtuhinahmed95@gmail.com>
2024-05-07 23:47:32 +0600

    dhakamart

--- DhakaMart/User.php
+++ DhakaMart/User.php
@@ -1,187 +1,125 @@
-<!-- <div class="container">
+<!-- Modals -->
+<!-- Add products modal -->
+<dialog id="my_modal_1" class="modal ">
+    <div class="modal-box " style="max-width:800px;">
+        <form method="post" action="ViewCode.php">
+            <!-- Image -->
+            
+            <label for="name"> Name</label><br>
+            <input name="name" type="text" id="name" class="input-field"><br>
 
-  <div>
-    <h1 class="text-center font-bold text-2xl">User Table</h1>
-  </div>
-  <div class="flex justify-end">
-    <i class='bx bxs-user-plus' style="font-size:36px;"></i>
-  </div>
-
-  <div class="user-table">
-    <table class="w-full">
-      <tr>
-        <th>#</th>
-        <th>Name</th>
-        <th>Username</th>
-        <th>user Role</th>
-        <th>Status</th>
-        <th>Last Login</th>
-        <th>Action</th>
-      </tr>
-      <tr>
-        <td>1</td>
-        <td>christopher</td>
-        <td>User</td>
-        <td>User</td>
-        <td class="active">Active</td>
-        <td>April 4-2024.7:12:12pm</td>
-        <td>
-          <div>
-            <i class='bx bx-edit-alt' style="color: #ffffff; background-color:rgb(55, 234, 55);font-size:26px;"></i>
-            <i class='bx bxs-message-alt-x' style="color: #ffffff; background-color:red;font-size:26px;"></i>
-
-          </div>
-        </td>
-      </tr>
-      <tr>
-        <td>2</td>
-        <td>Jhon Walker</td>
-        <td>Special</td>
-        <td>Special</td>
-        <td class="active">Active</td>
-        <td>April 4-2024.8:32:12pm</td>
-        <td>
-          <div>
-            <i class='bx bx-edit-alt' style="color: #ffffff; background-color:rgb(55, 234, 55);font-size:26px;"></i>
-            <i class='bx bxs-message-alt-x' style="color: #ffffff; background-color:red;font-size:26px;"></i>
-          </div>
-        </td>
-
-      </tr>
-      <tr>
-        <td>3</td>
-        <td>Kevin</td>
-        <td>Kevin</td>
-        <td>User</td>
-        <td class="active">Active</td>
-        <td>April 4-2024.8:45:12pm</td>
-        <td>
-          <div>
-            <i class='bx bx-edit-alt' style="color: #ffffff; background-color:rgb(55, 234, 55);font-size:26px;"></i>
-            <i class='bx bxs-message-alt-x' style="color: #ffffff; background-color:red;font-size:26px;"></i>
-          </div>
-        </td>
-
-      </tr>
-      <tr>
-        <td>4</td>
-        <td>Natia Williams</td>
-        <td>Natia</td>
-        <td>User</td>
-        <td class="active">Active</td>
-        <td>April 4-2024.9:10:12pm</td>
-        <td>
-          <div>
-            <i class='bx bx-edit-alt' style="color: #ffffff; background-color:rgb(55, 234, 55);font-size:26px;"></i>
-            <i class='bx bxs-message-alt-x' style="color: #ffffff; background-color:red;font-size:26px;"></i>
-          </div>
-        </td>
-
-      </tr>
-      <tr>
-        <td>5</td>
-        <td>Robin hood</td>
-        <td>Robin</td>
-        <td>User</td>
-        <td class="active">Active</td>
-        <td>April 4-2024.10:12:12pm</td>
-        <td>
-          <div>
-            <i class='bx bx-edit-alt' style="color: #ffffff; background-color:rgb(55, 234, 55);font-size:26px;"></i>
-            <i class='bx bxs-message-alt-x' style="color: #ffffff; background-color:red;font-size:26px;"></i>
-          </div>
-        </td>
-      </tr>
-
-      <tr>
-        <td>5</td>
-        <td>Akash Ahmed</td>
-        <td>Akash</td>
-        <td>User</td>
-        <td class="active">Active</td>
-        <td>April 4-2024.10:20:12pm</td>
-        <td>
-          <div>
-            <i class='bx bx-edit-alt' style="color: #ffffff; background-color:rgb(55, 234, 55);font-size:26px;"></i>
-            <i class='bx bxs-message-alt-x' style="color: #ffffff; background-color:red;font-size:26px;"></i>
-          </div>
-        </td>
-      </tr>
-
-      <tr>
-        <td>5</td>
-        <td>Shaheen Alam</td>
-        <td>Shaheen</td>
-        <td>User</td>
-        <td class="active">Active</td>
-        <td>April 4-2024.10:50:12pm</td>
-        <td>
-          <div>
-            <i class='bx bx-edit-alt' style="color: #ffffff; background-color:rgb(55, 234, 55);font-size:26px;"></i>
-            <i class='bx bxs-message-alt-x' style="color: #ffffff; background-color:red;font-size:26px;"></i>
-          </div>
-        </td>
-      </tr>
-
-      <tr>
-        <td>5</td>
-        <td>Ramjan Kabir</td>
-        <td>Kabir</td>
-        <td>User</td>
-        <td class="active">Active</td>
-        <td>April 4-2024.11:12:12pm</td>
-        <td>
-          <div>
-            <i class='bx bx-edit-alt' style="color: #ffffff; background-color:rgb(55, 234, 55);font-size:26px;"></i>
-            <i class='bx bxs-message-alt-x' style="color: #ffffff; background-color:red;font-size:26px;"></i>
-          </div>
-        </td>
-      </tr>
-
-      <tr>
-        <td>5</td>
-        <td>Ausub Khan</td>
-        <td>Ausub</td>
-        <td>User</td>
-        <td class="active">Active</td>
-        <td>April 4-2024.11:30:12pm</td>
-        <td>
-          <div>
-            <i class='bx bx-edit-alt' style="color: #ffffff; background-color:rgb(55, 234, 55);font-size:26px;"></i>
-            <i class='bx bxs-message-alt-x' style="color: #ffffff; background-color:red;font-size:26px;"></i>
-          </div>
-        </td>
-      </tr>
-
-    </table>
-
-  </div>
+            <label for="email">E-Mail</label><br>
+            <input name="email" type="text" id="name" class="input-field"><br>
 
 
+           
 
+            
+            <label for="cat_id">Role_User</label><br>
+            <select  class="select-field" name="cat_id" id="cat_id">
+                <?php
+                    $conn = mysqli_connect('localhost', 'root', '', 'dhakamart');
+                    $query = "SELECT * FROM user";
+                    $query_run = mysqli_query($conn,  $query);
+                    if (mysqli_num_rows($query_run)>0) {
+                    foreach ($query_run as $row) {
+                    ?>
+                    <option value="<?php echo $row['id']?>"><?php echo $row ['name']?></option>
+                    <?php
+                 }
+                }
+                ?>
+            
+            </select><br>
+            
+            
 
+            <div class="modal-action modal_btn">
+                <input type="text" name="test">
+                <!-- if there is a button in form, it will close the modal -->
+                <button class="btn" type="submit" name="add_sub_category">ADD</button>
+            </div>
 
-
-</div> -->
-
-
-<head> 
-  <body>
-    <center>
-    <div class="container" method="POST"> 
-      <form class="user-form" action="ViewCode.php" method="POST"> 
-        <label for="name">Name</label>
-        <input type="text" name="name"><br><br>
-
-        <label for="email">E-Mail</label>
-        <input type="text" name="email"><br><br>
-
-        <label for="role_id">Role_Id</label>
-        <input type="text" name="role_id"><br>
-
-        <input type="submit" name="user_btn" value="submit"> 
-      </form>
+        </form>
+        <form method="dialog">
+            <button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+        </form>
     </div>
-    </center>
-  </body>
-</head>
+</dialog>
+<!-- Add products modal end-->
+<!-- Modals End-->
+<!-- -------------------------------------Content start form here---------------------------------------------- -->
+<!-- Products table header -->
+<div class="flex gap-3 items-center justify-center my-5">
+    <div class="border-t flex-grow "></div>
+    <h1 class="text-center font-bold text-2xl">User Table</h1>
+    <div class="border-t flex-grow "></div>
+</div>
+<!-- Add Product -->
+<div class="flex justify-end my-4">
+    <button class="btn btn-md btn-secondary view-details " onclick="my_modal_1.showModal()">
+        <i class="fa-regular fa-square-plus"></i> Add User
+    </button>
+</div>
+<!-- Products Table -->
+<div class="">
+    <table class="custom-table">
+        <thead>
+            <tr>
+                <th>#ID</th>
+                <th>Name</th>
+                <th>E-Mail</th>
+                <th>Role_Id</th>
+                <th>Action</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            $conn = mysqli_connect('localhost', 'root', '', 'dhakamart');
+            if (!$conn) {
+                die("Connection failed: " . mysqli_connect_error());
+            }
+            $query = "SELECT user.id,user.name,user.email from user,role where user.role_id = role.id";
+            $query_run = mysqli_query($conn,  $query);
+            if (mysqli_num_rows($query_run) > 0) {
+                foreach ($query_run as $row) {
+            ?>
+                    <tr>
+                        <th><?php echo $row["id"] ?></th>
+                        
+                        <td><?php echo $row["name"] ?></td>
+                        <td><?php echo $row["name"] ?></td>
+                        <td><?php echo $row["email"] ?></td>
+                        <td>
+                            <!-- You can open the modal using ID.showModal() method -->
+                            <div class="tooltip" data-tip="Edit">
+
+                                <a href="Sub_CategoryEdit.php?id=<?php echo $row["id"] ?>"><i style="color:green; font-size:20px;" class="fa-regular fa-pen-to-square"></i></a>
+
+                            </div>
+                            <!-- Delete Button -->
+                            <div class="tooltip" data-tip="Delete">
+                                <form action="ViewCode.php" method="post">
+                                    <button type="submit" name="delete_sub_cat" value="<?php echo $row['id'] ?>"><i style="color:red; font-size:20px;" class="fa-regular fa-trash-can"></i></button>
+                                </form>
+                            </div>
+                            <!-- View details button -->
+                            <!-- <div class="tooltip" data-tip="Details">
+                                <button onclick="my_modal_2.showModal()">
+                                    <i style="color:#007bff; font-size:20px; margin-left:10px;" class="fa-regular fa-eye"></i>
+                                </button>
+                            </div> -->
+                        </td>
+                    </tr>
+
+            <?php
+                }
+            } else {
+
+                echo "<h1>No records found</h1>";
+            }
+            ?>
+
+        </tbody>
+    </table>
+</div>
---
+++ DhakaMart/user_edit.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="en" data-theme="light">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mart-Dhaka</title>
+    <!-- DaisyUI -->
+    <link href="https://cdn.jsdelivr.net/npm/daisyui@4.10.2/dist/full.min.css" rel="stylesheet" type="text/css" />
+    <script src="https://cdn.tailwindcss.com"></script>
+    <!-- Font awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" />
+    <!-- Animate css -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js">
+    </script>
+    <!-- custom css file -->
+    <style>
+        <?php include "style.css" ?>
+    </style>
+</head>
+
+<body>
+    <?php require("./Shared/Navbar.php"); ?>
+    <div class="flex">
+        <div class="w-[22%] min-h-screen bg-[#0E1428]">
+            <?php require("./Shared/Sidebar.php") ?>
+        </div>
+
+        <div class=" w-full mx-4 py-5">
+            <div class="w-3/6 mx-auto">
+                <?php
+                if (isset($_GET['id'])) {
+                    $sID = $_GET['id'];
+                    $conn = mysqli_connect('localhost', 'root', '', 'dhakamart');
+                    $query = "SELECT * FROM user WHERE id= '$sID' LIMIT 1";
+                    $query_run = mysqli_query($conn,  $query);
+                    if (mysqli_num_rows($query_run) > 0) {
+                        foreach ($query_run as $row) {
+                ?>
+                            <form action="ViewCode.php" method="post">
+                                <input name="id" value="<?php echo $row["id"] ?>" type="hidden" class="input-field"><br>
+                                <label for="name">User Name</label><br>
+
+
+                                <input name="name" value="<?php echo $row["name"] ?>" type="text" id="name" class="input-field"><br>
+
+
+                                <label for="brand_id">Brand</label><br>
+                                <select  class="select-field" name="brand_id" id="brand_id">
+                                    <?php
+                                        $conn = mysqli_connect('localhost', 'root', '', 'dhakamart');
+                                        $query = "SELECT * FROM brand";
+                                        $query_run = mysqli_query($conn,  $query);
+                                        if (mysqli_num_rows($query_run)>0) {
+                                        foreach ($query_run as $row) {
+                                        ?>
+                                        <option value="<?php echo $row['id']?>"><?php echo $row ['name']?></option>
+                                        <?php
+                                    }
+                                    }
+                                    ?>
+                                
+                                </select><br>
+
+            
+                                <label for="cat_id">Category</label><br>
+                                <select  class="select-field" name="cat_id" id="cat_id">
+                                    <?php
+                                        $conn = mysqli_connect('localhost', 'root', '', 'dhakamart');
+                                        $query = "SELECT * FROM category";
+                                        $query_run = mysqli_query($conn,  $query);
+                                        if (mysqli_num_rows($query_run)>0) {
+                                        foreach ($query_run as $row) {
+                                        ?>
+                                        <option value="<?php echo $row['id']?>"><?php echo $row ['name']?></option>
+                                        <?php
+                                    }
+                                    }
+                                    ?>
+                                </select><br>
+                            
+                                <div class="modal-action modal_btn">
+                                    <button class="btn" type="submit" name="update_sub_category">Update</button>
+                                </div>
+
+                            </form>
+                <?php
+                        }
+                    }
+                }
+                ?>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+<script src="index.js"></script>
+
+</html>
