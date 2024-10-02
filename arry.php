d67b66c539c250f7c44b23121d1d6f92babfe058
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-10-02 19:47:54 +0600

    php array

--- arry.php
+++ arry.php
@@ -2,13 +2,13 @@
 
 
 function fix_array($arr) {
-    // প্রথমে অ্যারে থেকে ডুপ্লিকেট সরিয়ে ফেলুন
+    
     $unique_arr = array_unique($arr);
     
-    // তারপর অ্যারেটিকে ঊর্ধ্বক্রমে সাজান
+   
     sort($unique_arr);
     
-    // সাজানো অ্যারেটি ফেরত দিন
+    
     return $unique_arr;
 }
 
