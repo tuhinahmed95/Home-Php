4d5421c1f709e52bf90e30be118d22497ebcd960
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-10-02 19:35:43 +0600

    php

--- arry.php
+++ arry.php
@@ -1,3 +1,13 @@
 <?php
 
-$arr = [ 3, 26, 1, 2, 3, 44, 57, 87, 1 ];
+// $array = [ 3, 26, 1, 2, 3, 44, 57, 87, 1 ];
+
+function fix_arry($array){ 
+
+    $unique_arry = array_unique($array);
+    sort($unique_arry);
+
+    return $unique_arry;
+}
+
+echo fix_arry($unique_arry);
