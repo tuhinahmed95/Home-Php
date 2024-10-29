dfe36ac0aa0571b9dcfb31a066b43cc96926cb51
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-10-29 14:08:45 +0600

    php

--- constant.php
+++ constant.php
@@ -7,9 +7,29 @@
 // define('first',40,true);
 // echo first;
 
-$x = 30;
-$y = 20;
-if($x<=>$y){ 
-    echo 'Correct';
+// $x = 30;
+// $y = "30";
+// if($x===$y){ 
+//     echo 'Correct';
+// }echo 'not correct';
+
+// $marks = 0;
+
+// if($marks>=80 && $marks<=100){ 
+//     echo 'Your Are In A Merit';
+// }elseif($marks>=70 && $marks<=79){ 
+//     echo 'Your Are In A ';
+// }elseif($marks>=60 && $marks<=69){ 
+//     echo 'Your Are In A B';
+// }else{ 
+//     echo "Please Enter Valid number";
+// }
+
+$a = 0;
+while($a<20){ 
+    echo "Tuhin.<br/>";
+    $a++;
 }
+
+
 ?>
