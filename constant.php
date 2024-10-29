88f0a3a209b71cffa80ecff36355d9a87618e3a8
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-10-29 13:05:38 +0600

    php

--- constant.php
+++ constant.php
@@ -1,5 +1,15 @@
 <?php
 
-define('name','tuhin');
-echo name;
+// define('first',60,true);
+// $sum = first + 30;
+// echo " Sum = " . $sum;
+
+// define('first',40,true);
+// echo first;
+
+$x = 30;
+$y = 20;
+if($x<=>$y){ 
+    echo 'Correct';
+}
 ?>
