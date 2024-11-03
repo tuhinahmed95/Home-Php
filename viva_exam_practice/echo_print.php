7856c1ca92410c5b561e69c390e50623002a1f89
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-11-03 12:52:04 +0600

    php

--- viva_exam_practice/echo_print.php
+++ viva_exam_practice/echo_print.php
@@ -10,10 +10,26 @@
 // $string2 = (string)$string;
 // echo $string2;
 
-$number = "123.59";
-$foat = (float)$number;
-echo $foat;
+// $number = "123.59";
+// $foat = (float)$number;
+// echo $foat;
 
-$vari = "123";
-$string = (string)$vari;
-echo $string;
+// $vari = "123";
+// $string = (string)$vari;
+// echo $string;
+
+// $bol = 10;
+// $bolvalue = (string)$bol;
+// echo $bolvalue;
+
+// $number = 123;
+// $string_number = (string)$number;
+// echo $string_number; 
+
+// $name = 'hello';
+// $array = (array)$name;
+// print_r ($array);
+
+$names = ['name'=>'tuhin','age'=>30];
+$object = (object)$names;
+echo $object;
