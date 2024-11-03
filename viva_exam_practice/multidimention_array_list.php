571335c179025aaba873d241dcf623de2439c939
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-11-04 00:01:39 +0600

    php

---
+++ viva_exam_practice/multidimention_array_list.php
@@ -0,0 +1,47 @@
+<?php
+
+// $emp = [
+//     [1,'Tuhin','Developer',78000],
+//     [2,'Rafia','Sofware Enginier',40000],
+//     [3,'Fariha','Senior Enginier',45500],
+//     [4,'Anika','BCS',35000],
+// ];
+echo "<table border='2px' cellpadding='10px' cellspacing='0'> 
+    <tr>
+        <th>Id</th>
+        <th>Name</th>
+        <th>Designation</th>
+        <th>Salary</th>
+    </tr>
+";
+
+$emp = [
+    [
+        "Id"=>1,
+        "Name"=>"Tuhin",
+        "Designation"=>"Senior Developer",
+        "salary"=>80000,
+    ],
+    [
+        "Id"=>2,
+        "Name"=>"Tasrif",
+        "Designation"=>"Software Enginier",
+        "salary"=>70000,
+    ],
+    [
+        "Id"=>3,
+        "Name"=>"Babu",
+        "Designation"=>"Bussiness",
+        "salary"=>20000,
+    ],
+];
+foreach($emp as list("Id" =>$id, "Name" =>$name,"Designation" =>$designation,"salary" =>$salary)){ 
+    // foreach($emp as list($id,$name,$designation,$salary)){ 
+    echo " <tr> 
+            <td>$id</td>
+            <td>$name</td>
+            <td>$designation</td>
+            <td>$salary</td>
+        </tr>";
+}
+echo "<table/>";
--- viva_exam_practice/multidimention_assosiative_array.php
+++ viva_exam_practice/multidimention_assosiative_array.php
@@ -4,8 +4,16 @@ $marks = [
     "Tuhin"=>['physics'=>80,'chemistry'=>89,'math'=>96],
     "Rafia"=>['physics'=>78,'chemistry'=>78,'math'=>90],
     "Salman"=>['physics'=>87,'chemistry'=>67,'math'=>78],
+    "Shahrukh"=>['physics'=>56,'chemistry'=>87,'math'=>80],
 ];
-echo "<table border='2px' cellpadding='5px' cellspacing='0'>";
+echo "<table border='2px' cellpadding='5px' cellspacing='0'>
+        <tr>
+            <th>Student Name</th>
+            <th>Physics</th>
+            <th>Chemistr</th>
+            <th>Math</th>
+        </tr>
+";
 foreach($marks as $key => $value1){ 
     echo "<tr>";
     echo "<td>$key</td>";
