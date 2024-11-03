899e2e73032178abe11954b1ba0d1176e080abb4
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-11-03 12:38:40 +0600

    php

--- viva_exam_practice/echo_print.php
+++ viva_exam_practice/echo_print.php
@@ -1,6 +1,19 @@
 <?php
 
-$number = '123';
+// $number = '123';
 
-$int = (int)$number;
-echo $int;
+// $int = (int)$number;
+// echo $int;
+
+// $string = "tuhin";
+
+// $string2 = (string)$string;
+// echo $string2;
+
+$number = "123.59";
+$foat = (float)$number;
+echo $foat;
+
+$vari = "123";
+$string = (string)$vari;
+echo $string;
