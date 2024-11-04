07c6d3db5cd02a38bac613b0fb79722144eb5fe6
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-11-04 14:46:36 +0600

    php

--- viva_exam_practice/date_time.php
+++ viva_exam_practice/date_time.php
@@ -1,6 +1,13 @@
 <?php
 
 
-$date = date_create("2024-11-4");
-date_time_set($date, 12,40);
-echo date_format($date, "y-m-d | H:i:s");
+// $date = date_create("2024-11-4");
+// date_time_set($date, 12,40);
+// echo date_format($date, "y-m-d | H:i:s"); 
+// echo"<br/>";
+// echo date_default_timezone_get();
+
+echo "PHP_SELF :" . $_SERVER['PHP_SELF']."<br>";
+echo "SERVER_NAME". $_SERVER['SERVER_NAME']."<br>";
+echo "HTTP_HOST". $_SERVER['HTTP_HOST']."<br>";
+echo "REQUEST_METHOD".$_SERVER['REQUEST_METHOD']."<br>";
