06a21df3f8ee0aa97c091daf1d9003161e8aad6f
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-11-05 13:35:09 +0600

    php

--- viva_exam_practice/OOP/class_properties_mehod.php
+++ viva_exam_practice/OOP/class_properties_mehod.php
@@ -1,17 +1,19 @@
 <?php
  class car{ 
-    public $name='lambargini';
-    public $model = 'mt3o';
-    public $price = 499999;
+    public $name;
+    public $model;
+    public $price;
 
-    function cardetails(){ 
-        echo "This is a example";
+    function cardetails($cname,$cmodel,$cprice){ 
+        $this->name = $cname;
+        $this->model = $cmodel;
+        $this->price = $cprice;
+        return "Car Name :".$this->name.",Car Model :".$this->model.",CarPrice :".$this->price;
     }
  }
 
  $carinfo = new car();
- echo $carinfo->name = 'lambargini';
- echo $carinfo->price;
+ echo $carinfo->cardetails('lambarginit','mtee99',39000000);
 
 
 ?>
