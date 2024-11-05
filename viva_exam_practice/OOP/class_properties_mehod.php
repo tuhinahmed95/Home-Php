d654b6d4b993d19ecd34304260f11e2a8406cd24
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-11-05 13:39:37 +0600

    php

--- viva_exam_practice/OOP/class_properties_mehod.php
+++ viva_exam_practice/OOP/class_properties_mehod.php
@@ -14,6 +14,11 @@
 
  $carinfo = new car();
  echo $carinfo->cardetails('lambarginit','mtee99',39000000);
+echo "<br>";
+ echo $carinfo->cardetails('Audi','rer55',788888);
+ echo "<br>";
+
+ echo $carinfo->cardetails('BMW','989rrrr55',345678);
 
 
 ?>
