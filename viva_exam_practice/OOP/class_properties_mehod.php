78afa1785ce07d3a10aea63037e359921772cea1
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-11-05 14:24:24 +0600

    php

--- viva_exam_practice/OOP/class_properties_mehod.php
+++ viva_exam_practice/OOP/class_properties_mehod.php
@@ -1,24 +1,44 @@
 <?php
- class car{ 
+//  class car{ 
+//     public $name;
+//     protected $model;
+//     public $price;
+
+//     function cardetails($cname,$cmodel,$cprice){ 
+//         $this->name = $cname;
+//         $this->model = $cmodel;
+//         $this->price = $cprice;
+//         return "Car Name :".$this->name.",Car Model :".$this->model.",CarPrice :".$this->price;
+//     }
+//  }
+
+//  $carinfo = new car();
+//  echo $carinfo->cardetails('lambarginit','mtee99',39000000);
+//  echo "<br>";
+//  echo $carinfo->cardetails('Audi','rer55',788888);
+//  echo "<br>";
+//  echo $carinfo->cardetails('BMW','989rrrr55',345678);
+//  echo "<br>";
+//  $information = new car();
+//  echo $information->cardetails('rangerove','rty66',6788900);
+
+class man { 
     public $name;
-    public $model;
-    public $price;
+    public $age;
+    public $address;
 
-    function cardetails($cname,$cmodel,$cprice){ 
-        $this->name = $cname;
-        $this->model = $cmodel;
-        $this->price = $cprice;
-        return "Car Name :".$this->name.",Car Model :".$this->model.",CarPrice :".$this->price;
+    function __construct($fname,$fage,$fadd){ 
+        $this->name = $fname;
+        $this->age = $fage;
+        $this->address = $fadd;
     }
- }
 
- $carinfo = new car();
- echo $carinfo->cardetails('lambarginit','mtee99',39000000);
-echo "<br>";
- echo $carinfo->cardetails('Audi','rer55',788888);
- echo "<br>";
+    function info(){ 
+        return "Fname :".$this->name. "Fage :". $this->age. "Address : ".$this->address;
+    }
+}
 
- echo $carinfo->cardetails('BMW','989rrrr55',345678);
-
+$object = new man('Tuhin',30,'Dhaka');
+echo $object->info();
 
 ?>
