af88ce4959b2f9bfe8558f96ad2486a521339443
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-11-06 22:54:18 +0600

    php

--- viva_exam_practice/static_method.php
+++ viva_exam_practice/static_method.php
@@ -2,19 +2,22 @@
 
 
 class User{ 
+
+    public static $name = "Babu";
     public static function UserName (){ 
         return "Tuhin Ahmed";
     }
 
     public static function UserAge(){ 
-        return 39 . self::UserName();
+        return 39 . " ".self::UserName().self::$name;
     }
 }
 
-class Post { 
+class Post extends User{ 
     public static function postCreate(){ 
         return "This is Child Class" . User::UserAge();
     }
 }
 
 echo Post::postCreate();
+ 
