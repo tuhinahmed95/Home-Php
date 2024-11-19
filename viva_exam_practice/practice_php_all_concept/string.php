63ec400236726636fc4f9c8e86baead0ccb3f11f
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-11-19 11:21:45 +0600

    php

--- viva_exam_practice/practice_php_all_concept/string.php
+++ viva_exam_practice/practice_php_all_concept/string.php
@@ -4,7 +4,17 @@ $string = 'Tuhin Ahmed';
 // echo strtolower($string);
 // echo strtoupper($string);
 
-echo str_replace('Ahmed','Khan',$string);
+// echo str_replace('Ahmed','Khan',$string);
+
+// echo strrev($string);
+// $array = explode(" ", $string);
+// print_r($array);
+
+// $array = ['tuhin','ahmed','montu','jhontu'];
+// $string = implode(' ', $array);
+// echo $string;
+
+echo trim($string);
 
 
 ?>
