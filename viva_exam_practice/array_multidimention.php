20518292637b2e4fb4f9d486326422c48863a925
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-11-20 16:49:48 +0600

    php

--- viva_exam_practice/array_multidimention.php
+++ viva_exam_practice/array_multidimention.php
@@ -1,17 +1,25 @@
 <?php
 
-$numbers = [23,45,67,78,89];
+// $numbers = [23,45,67,78,89];
 
 // foreach($numbers as $number){ 
 //     echo $number;
 // }
 
-function myfunction (){ 
-    foreach($numbers as $number){ 
-        echo $number;
-    }
-}
+// function myfunction (){ 
+//     foreach($numbers as $number){ 
+//         echo $number;
+//     }
+// }
 
-myfunction();
+// myfunction();
+
+$x = 0;
+
+while($x<6){ 
+    $x++;
+    if($x == 3) continue;
+    echo $x;
+}
 
 ?>
