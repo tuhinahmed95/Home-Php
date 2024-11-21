b6b6620e4ecaedd2643678a5a0cff5fb1d32c815
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-11-21 11:30:32 +0600

    php

---
+++ viva_exam_practice/practice_array.php
@@ -0,0 +1,12 @@
+<?php
+
+$cars = ['volvo','audi','bmw'];
+$cars [1] = "lambargini";
+print_r($cars)."<br/>";
+
+$bikes = ['pulser','Royal Enfield','zixer','R15','Xblade'];
+foreach($bikes as $bike){ 
+    echo $bike. " ";
+}
+
+?>
