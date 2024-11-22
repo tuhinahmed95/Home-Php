e971e968a55751e1a6c0c82a388621ccc41af926
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-11-23 00:27:14 +0600

    php

--- viva_exam_practice/practice_php_all_concept/add_array.php
+++ viva_exam_practice/practice_php_all_concept/add_array.php
@@ -1,7 +1,8 @@
 <?php
 
 $arrays = ['volvo','toyota','bmw'];
-$arrays [] = 'Lambergint';
+// $arrays [] = 'Lambergint';
+unset($arrays[0],$arrays[2]);
 // array_unshift($arrays,'Honey');
 // array_splice($arrays,1,2);
 // unset($arrays[1]);
@@ -47,5 +48,65 @@ echo "<br/>";
 $color = ['first'=>'red','second'=>'green','third'=>'yello','four'=>'blue'];
 $newarray = array_diff($color,['red','green','yello']);
 print_r($newarray);
+echo "<br/>";
 
+$numbers = [2,5,8,9,70];
+rsort($numbers);
+$arrylengt = count($numbers);
+for($x=0;$x<$arrylengt;$x++){ 
+    echo $numbers[$x]."<br/>";
+}
+
+echo "<br/>";
+
+$strings = ['first'=>"number","second"=>"number-9","third"=>"number-2"];
+krsort($strings);
+foreach($strings as $key => $string){ 
+    echo "Key = ".$key. "Value = ".$string."<br/>";
+}
+
+echo "<br>";
+
+$numbers = array(2,5,8,1,6,9);
+rsort($numbers);
+$arraylength = count($numbers);
+for($x = 0; $x < $arraylength; $x++){ 
+    echo $numbers[$x]."<br/>";
+}
+
+echo "<br/>";
+
+$strings = array('name'=>'Audi','model'=>'monaco45','color'=>"red",'price'=>456788);
+krsort($strings);
+foreach($strings as $key => $string){ 
+    echo "Key = " . $key . "Value = ".$string. "<br/>";
+}
+
+echo "<br/>";
+
+$cars = [
+    ['volvo',22,18],
+    ['toyota',34,23],
+    ['bmw',15,9]
+];
+
+for($row = 0; $row < 3; $row++){ 
+    echo "<p>Number Of Row = $row</p>";
+    echo "<ul>";
+    for($col = 0; $col < 3; $col++){ 
+        echo "<li>".$cars[$row][$col]."<li/>";
+    }
+    echo "<ul/>";
+}
+
+echo "<br/>";
+
+$x = 100;
+
+echo $GLOBALS['x'];
+echo $x;
+
+echo "<br/>";
+
+echo $_SERVER['HTTP_HOST'];
 ?>
