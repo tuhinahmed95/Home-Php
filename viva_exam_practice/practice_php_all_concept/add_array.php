910103cabb4feb427eb95138784b7a6896f46bba
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-11-22 20:28:53 +0600

    php

--- viva_exam_practice/practice_php_all_concept/add_array.php
+++ viva_exam_practice/practice_php_all_concept/add_array.php
@@ -1,9 +1,12 @@
 <?php
 
 $arrays = ['volvo','toyota','bmw'];
+// array_unshift($arrays,'Honey');
 // array_splice($arrays,1,2);
 // unset($arrays[1]);
-print_r($arrays[0]);
+// $arrays[0] = 'Kawasaki';
+// array_splice($arrays,1,2);
+print_r($arrays[1]);
 // var_dump($arrays);
 
 echo "<br/>";
@@ -11,5 +14,29 @@ echo "<br/>";
 $cars = ['NUMBER-1'=>'BMW','NUMBER-2'=>'AUDI','NUMBER-3'=>'TOYOTA','NUMBER-4'=>'MERCEDIES'];
 print_r($cars);
 var_dump($cars);
+
+echo "<br/>";
+
+function myfunction(){ 
+    echo "I come from Function";
+}
+
+// $array = ['tuhin','ahmed',myfunction()];
+// $array [2]();
+// echo "<br/>";
+
+// function Mfunction(){ 
+//     echo "This Is A function";
+// }
+
+$cars = ['name'=>'Audi','color'=>'red','price'=>35999];
+// array_push($cars,'brnd','higclass');
+// array_unshift($cars,'first','Brand');
+array_shift($cars);
+// $cars ['name'] = 'Lambergini';
+//  $cars ['message'];
+// echo $cars['name'];
+// print_r($cars['name']);
+print_r($cars);
 
 ?>
