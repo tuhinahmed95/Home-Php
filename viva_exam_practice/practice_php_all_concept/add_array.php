0822fc7c875b22fd296859987734db652ebcde26
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-11-22 19:55:21 +0600

    php

---
+++ viva_exam_practice/practice_php_all_concept/add_array.php
@@ -0,0 +1,15 @@
+<?php
+
+$arrays = ['volvo','toyota','bmw'];
+// array_splice($arrays,1,2);
+// unset($arrays[1]);
+print_r($arrays[0]);
+// var_dump($arrays);
+
+echo "<br/>";
+
+$cars = ['NUMBER-1'=>'BMW','NUMBER-2'=>'AUDI','NUMBER-3'=>'TOYOTA','NUMBER-4'=>'MERCEDIES'];
+print_r($cars);
+var_dump($cars);
+
+?>
