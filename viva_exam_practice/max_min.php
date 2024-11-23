1204f5e1cc16d71b45154e0c90036818c7de94dd
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-11-23 22:01:29 +0600

    php

---
+++ viva_exam_practice/max_min.php
@@ -0,0 +1,16 @@
+<?php
+$numbers = [3,7,8,9];
+echo (max($numbers));
+echo "<br/>";
+echo (min($numbers));
+
+echo "<br/>";
+
+$arrays = ['Tuhin','Ahmed','Rafia','Kabir','Khan'];
+foreach($arrays as $array){ 
+    echo $array."<br/>";
+}
+echo $arrays[0];
+// var_dump($arrays);
+
+?>
