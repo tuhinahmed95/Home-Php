736daaecc96efba0cd8097f1a232ae954fb632ca
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-11-23 22:08:04 +0600

    php

--- viva_exam_practice/max_min.php
+++ viva_exam_practice/max_min.php
@@ -7,10 +7,18 @@ echo (min($numbers));
 echo "<br/>";
 
 $arrays = ['Tuhin','Ahmed','Rafia','Kabir','Khan'];
+rsort($arrays);
 foreach($arrays as $array){ 
     echo $array."<br/>";
 }
 echo $arrays[0];
 // var_dump($arrays);
+echo "<br/>";
+
+$strings = array("name"=>"Tuhin",'Profession'=>"Developer","Address"=>"Gaibandha","Email"=>"mtuhinahmed95@gmail.com");
+krsort($strings);
+foreach($strings as $key=>$string){ 
+    echo "Key = ".$key. "Values =".$string. "<br/>";
+}
 
 ?>
