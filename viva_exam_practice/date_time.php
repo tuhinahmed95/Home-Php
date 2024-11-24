89fdc70a867c16da3d064426941a8cb8ac08b284
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-11-24 17:17:56 +0600

    php

--- viva_exam_practice/date_time.php
+++ viva_exam_practice/date_time.php
@@ -11,3 +11,14 @@ echo "PHP_SELF :" . $_SERVER['PHP_SELF']."<br>";
 echo "SERVER_NAME". $_SERVER['SERVER_NAME']."<br>";
 echo "HTTP_HOST". $_SERVER['HTTP_HOST']."<br>";
 echo "REQUEST_METHOD".$_SERVER['REQUEST_METHOD']."<br>";
+
+echo "<br/><br/>";
+echo "Today is = ". date("y/m/d");
+echo "<br/>";
+echo "Current Year = ". date("Y");
+echo "<br/>";
+echo "Today = ". date("d");
+echo "<br/>";
+echo "Current Month = ".date("m");
+echo "<br/>";
+echo "Current day = ".date("d");
