0f77cce935fa142153ecafa4bf9b8346b8eca331
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-11-26 11:23:46 +0600

    php

--- viva_exam_practice/cokie_php.php
+++ viva_exam_practice/cokie_php.php
@@ -1,46 +1,33 @@
-
 <?php
 $c_name = "Cookie_Name";
 $c_value = "Cookie_Value";
 
-setcookie($c_name,$c_value,time()+ (86400 * 30), "/");
-setcookie('user', "",time()-3600);
+setcookie($c_name,$c_value,time()+ (86400*30),"/");
+
+if(isset($_COOKIE[$c_name])){ 
+    echo "Cookie ".$c_name. "Is Not Set";
+}else{ 
+    echo "Cookie Is ".$c_name ."Set";
+    echo "Value ".$_COOKIE[$c_name];
+}
+echo "<br/>";
+
+setcookie('user','Cookie_Value',time()-3600);
+if(count($_COOKIE)>0){ 
+    echo "Cookie is deleted";
+}
+echo "<br/>";
+
+setcookie('user','',time()+3600,'/');
+echo "Cookie 'user' is deleted";
+
+echo "<br/>";
+setcookie('test_cookie','test',time()+3600,'/');
+
+if(count($_COOKIE)>0){ 
+    echo "Cookie is enable";
+}else{ 
+    echo "Cookie is disable";
+}
 
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <?php
-    if(!isset($_COOKIE[$c_name])){ 
-        echo "Cookie Name'". $c_name ." 'is Not set";
-    }else{ 
-        echo "Cookie '". $c_name . "'Is Set";
-        echo "Value ". $_COOKIE[$c_name];
-    }
-
-    echo "<br/>";
-
-    setcookie('user','set_cokie',time()+3600);
-    if(count($_COOKIE)>0){ 
-        echo "cookie is enable";
-    }else{ 
-        echo "cookie is disable";
-    }
-
-    echo "<br/>";
-    echo "user is deleted"
-    
-    ?>
-
-
-</body>
-</html>
-
-
-
