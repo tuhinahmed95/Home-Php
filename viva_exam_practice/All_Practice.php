0abe43a43b9a756cde1d763e134572694bcc9e85
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-11-29 21:09:01 +0600

    php

--- viva_exam_practice/All_Practice.php
+++ viva_exam_practice/All_Practice.php
@@ -11,5 +11,28 @@ $arrays = ['tuhin','aryan','salman','amir'];
 // $str = (string)$arrays;
 $strr = implode(" ,", $arrays);
 echo $strr;
+echo "<br/>";
+echo "<br/>";
 
+$str = "tuhin Ahmed Gaibandha";
+$array = (array)$str;
+print_r($array);
+echo "<br/>";
+$bol = true;
+$ar = (array)$bol;
+print_r($ar);
+echo "<br/>";
+echo (pi());
+echo "<br/>";
+$math = [34,56,2,78,34,78];
+echo (max($math));
+echo "<br/>";
+echo (abs(-7.8));
+echo "<br/>";
+$n = 64;
+echo (sqrt($n));
+echo "<br/>";
+echo (rand(1,9));
+echo "<br/>";
+echo (round(5.4));
 ?>
