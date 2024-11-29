e70232ed45ec4d5edc6c29d9c867166a52506b46
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-11-29 21:00:00 +0600

    php

---
+++ viva_exam_practice/All_Practice.php
@@ -0,0 +1,15 @@
+<?php
+// practice castig
+$string = "php_casting practice";
+$int = (int)$string;
+echo $int;
+// var_dump($int);
+echo "<br/>";
+echo "<br/>";
+
+$arrays = ['tuhin','aryan','salman','amir'];
+// $str = (string)$arrays;
+$strr = implode(" ,", $arrays);
+echo $strr;
+
+?>
