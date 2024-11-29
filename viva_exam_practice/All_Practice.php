2d89bdac983fb4889de97c3b73251dc6fb8bf38b
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-11-29 22:02:38 +0600

    php

--- viva_exam_practice/All_Practice.php
+++ viva_exam_practice/All_Practice.php
@@ -35,4 +35,64 @@ echo "<br/>";
 echo (rand(1,9));
 echo "<br/>";
 echo (round(5.4));
+echo "<br/>";
+
+define('name','values');
+echo name;
+echo "<br/>";
+
+define('cars',[
+    'BMW','Volvo','Mercidies'
+]);
+echo cars[2];
+
+echo "<br/>";
+
+$string = '{"name":"Tuhin","Profession":"Developer","Address":"Gaibandha"}';
+$arry = json_decode($string);
+print_r($arry);
+echo "<br/>";
+$arr = ['tuhin','ahmed','hello','world'];
+$json = json_encode($arr);
+echo $json;
+echo "<br/>";
+
+function CalBack($item){ 
+    return strlen($item);
+}
+
+$ary = ['tuhin','ahmed','tasrif'];
+$str = array_map("CalBack",$arr);
+print_r($str);
+echo "<br/>";
+
+$tuna = ['tanvir','aslam','mister','coconut'];
+$str = array_map(function($item){return strlen($item);},$tuna);
+print_r($str);
+
+echo "<br/>";
+function MyFunction($items){ 
+    return strlen($items);
+}
+
+$a = ['fruits','coconut','orrange','apple'];
+$lent = array_map("MyFunction",$a);
+print_r($lent);
+
+echo "<br/>";
+echo "<br/>";
+
+$names = "User";
+$value = "Cokkie_Vlaue_Ales";
+
+setcookie($names,$value, time()+ 86400*30 , "/");
+
+if(!isset($_COOKIE[$names])){ 
+    echo "Cokkies Name ". $names . " Is Not Set ". "<br/>";
+}else { 
+    echo "Cokkie Name " . $names . " Is Set"."<br/>";
+    echo "Cokkie Value " . $_COOKIE[$names];
+}
+
+
 ?>
