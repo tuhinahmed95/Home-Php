18a4db9aa17eaa301acc7c863e62b28c89647aef
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-12-07 22:29:48 +0600

    php

--- viva_exam_practice/preaparation_for_logic.php
+++ viva_exam_practice/preaparation_for_logic.php
@@ -3,5 +3,16 @@
 /*echo "hello";
 echo "hello";*/
 
-$x = $y = $z = 'fruits';
-echo $x;
+// $x = $y = $z = 'fruits';
+// echo $x;
+/*echo "<br/>";
+echo "<br/>";*/
+$x = 5;
+$y = 10;
+
+function MyFunction(){ 
+    global $x,$y;
+    $y = $x + $y;
+}
+MyFunction();
+echo $y;
