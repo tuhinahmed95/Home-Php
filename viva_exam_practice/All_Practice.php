3b03fc3654e5c7dec8cf9b91f13b8e8854f27fb8
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-12-21 23:08:30 +0600

    php

--- viva_exam_practice/All_Practice.php
+++ viva_exam_practice/All_Practice.php
@@ -1,11 +1,5 @@
 <?php
 // practice castig
-$string = "php_casting practice";
-$int = (int)$string;
-echo $int;
-// var_dump($int);
-echo "<br/>";
-echo "<br/>";
 
 $arrays = ['tuhin','aryan','salman','amir'];
 // $str = (string)$arrays;
