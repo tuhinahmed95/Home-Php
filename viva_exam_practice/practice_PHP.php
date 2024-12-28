158ac43b9835723142e30d8f1881bdcc540696f1
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-12-28 10:24:30 +0600

    php

---
+++ viva_exam_practice/practice_PHP.php
@@ -0,0 +1,12 @@
+<?php
+
+function CallBack($item){ 
+    return strlen($item);
+}
+
+$array = ['banana','mango','pineapple','orragne'];
+
+$new = array_map("CallBack",$array);
+print_r($new);
+
+?>
