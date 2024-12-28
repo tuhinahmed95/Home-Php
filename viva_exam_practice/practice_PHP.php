7576167f4782a042f67e3bf7479fef981b49aa9e
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-12-28 10:42:33 +0600

    php

--- viva_exam_practice/practice_PHP.php
+++ viva_exam_practice/practice_PHP.php
@@ -15,4 +15,30 @@ echo "<br/>";
 $number = 45.678;
 $n =( is_float($number));
 var_dump($n);
+
+echo "<br/>";
+echo "<br/>";
+
+$cname = "User";
+$cvalue = 'John';
+setcookie($cname,$cvalue,time() + (86000*30),"/");
+
+
+
+
 ?>
+
+<html>
+<body>
+    <?php
+    if(!isset($_COOKIE[$cname])){ 
+        echo " Cookie name ". $cname . " Is Not Set";
+    }else{ 
+        echo "Cookie ". $cname . " is Set";
+        echo "value " . $cvalue . " Set";
+    }
+
+
+    ?>
+</body>
+</html>
