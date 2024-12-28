deeeb56633a806774266f43caa41dcb2bdb7fe08
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-12-28 10:25:53 +0600

    php

--- viva_exam_practice/practice_PHP.php
+++ viva_exam_practice/practice_PHP.php
@@ -1,12 +1,13 @@
 <?php
 
-function CallBack($item){ 
-    return strlen($item);
-}
+// function CallBack($item){ 
+//     return strlen($item);
+// }
 
 $array = ['banana','mango','pineapple','orragne'];
 
-$new = array_map("CallBack",$array);
-print_r($new);
+// $new = array_map("CallBack",$array);
+// print_r($new);
+$new = (array_map())
 
 ?>
