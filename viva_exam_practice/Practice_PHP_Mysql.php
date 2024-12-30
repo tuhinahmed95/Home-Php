12db64b94894df1f06a8567cbffb3a027f150740
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-12-30 10:26:24 +0600

    php

--- viva_exam_practice/Practice_PHP_Mysql.php
+++ viva_exam_practice/Practice_PHP_Mysql.php
@@ -1,9 +1,14 @@
 <?php 
 
 $array = [45,23,78,90,5,0];
-$lent = count($array);
-for($x = 0; $x < $lent; $x++){ 
-    if($array[$x] == 90)continue;
-    echo $array[$x]." ";
+// $lent = count($array);
+// for($x = 0; $x < $lent; $x++){ 
+//     if($array[$x] == 90)continue;
+//     echo $array[$x]." ";
+// }
+
+foreach($array as $arr){ 
+    if($arr == 0)continue;
+    echo $arr . " ";
 }
 ?>
