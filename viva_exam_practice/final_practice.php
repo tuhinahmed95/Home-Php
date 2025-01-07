f90a4ffd9b94f9ac4ce1ac3efd95e8410c9ad4cd
tuhinahmed95 <mtuhinahmed95@gmail.com>
2025-01-07 10:58:51 +0600

    php

---
+++ viva_exam_practice/final_practice.php
@@ -0,0 +1,18 @@
+<?php 
+
+$names = "Tuhin Ahmed";
+// echo strlen($names)."<br>";
+echo str_replace('Ahmed','khan',$names)."<br>";
+// echo str_word_count($names);
+echo "<br/>";
+echo "<br/>";
+
+$n = " Abir Mondol";
+echo trim($n);
+echo "<br/>";
+echo "<br/>";
+$na = "Tuhin Ahmed";
+echo substr($na, 5,5);
+
+
+?>
